2890af330912de0b49400fc98cd5a8f3fcfe84a1
Irith Katiyar <imkatiya@usc.edu>
2025-02-21 13:05:23 -0800

    Mind map controls and minimap added

--- automap/src/components/mind-map.tsx
+++ automap/src/components/mind-map.tsx
@@ -1,32 +1,83 @@
 "use client";
 
-import React, { forwardRef } from "react";
-import { ReactFlow, Background, BackgroundVariant } from '@xyflow/react';
+import React, { forwardRef, useCallback } from "react";
+import {
+    ReactFlow,
+    Background, 
+    BackgroundVariant,
+    Controls,
+    MiniMap,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    getIncomers,
+    getOutgoers,
+    getConnectedEdges,
+  } from '@xyflow/react';  
 import {  } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 const initialNodes = [
     { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+    { id: '3', position: { x: 0, y: 200 }, data: { label: '3' } },
 ];
 const initialEdges = [
-    { id: 'e1-2', source: '1', target: '2' }
+    { id: 'e1-2', source: '1', target: '2' },
+    { id: 'e2-3', source: '2', target: '3' },
 ];
 
 const MindMap = forwardRef<HTMLDivElement>((_, ref) => {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    
+    const onConnect = useCallback(
+        (params: any) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
+    
+    const onNodesDelete = useCallback(
+        (deleted: any[]) => {
+          setEdges(
+            deleted.reduce((acc: any[], node: any) => {
+              const incomers = getIncomers(node, nodes, edges);
+              const outgoers = getOutgoers(node, nodes, edges);
+              const connectedEdges = getConnectedEdges([node], edges);
+     
+              const remainingEdges = acc.filter(
+                (edge: { id: string; source: string; target: string; }) => !connectedEdges.includes(edge),
+              );
+     
+              const createdEdges = incomers.flatMap(({ id: source }) =>
+                outgoers.map(({ id: target }) => ({
+                  id: `${source}->${target}`,
+                  source,
+                  target,
+                })),
+              );
+     
+              return [...remainingEdges, ...createdEdges];
+            }, edges),
+          );
+        },
+        [nodes, edges],
+    );
 
     return (
         <div style={{ width: '100%', height: '100%'}}>
             <ReactFlow 
                 ref={ref}
-                nodes={initialNodes}
-                edges={initialEdges}
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                onNodesDelete={onNodesDelete}
                 proOptions={{hideAttribution: true}}
             >
-                <Background 
-                    color='#ccc' 
-                    variant={BackgroundVariant.Dots} 
-                />
+                <Controls />
+                <MiniMap />
+                <Background color='#ccc' variant={BackgroundVariant.Dots} gap={12} size={1} />
             </ReactFlow>
         </div>
     );
