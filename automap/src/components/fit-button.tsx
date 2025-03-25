ea5e3673a0022d822788ca489c2c6afb0c019171
Irith Katiyar <imkatiya@usc.edu>
2025-03-25 12:00:34 -0700

    Fit button dropdown

--- automap/src/components/fit-button.tsx
+++ automap/src/components/fit-button.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Crosshair } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu" 
+import {
     useReactFlow,
 } from '@xyflow/react';
 
@@ -9,13 +15,23 @@ import {
 const FitButton = () => {
     const { fitView } = useReactFlow();
     const fit = () => {
-        fitView({ padding: 0.2 });
+        fitView({ padding: 0.2, duration: 500 });
+    };
+    
+    const layout = () => {
+
     };
 
     return (
-        <Button className="bg-black hover:bg-blue-600 transition-all h-9 px-4 py-2 m-1 text-white outline-none shadow-none rounded-md text-sm font-medium" onClick={fit}>
-            <Crosshair size={15} />
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="bg-black hover:bg-blue-600 text-white transition-all h-9 px-4 py-2 m-1 rounded-md text-sm font-medium outline-none shadow-none ">
+                <Crosshair size={15} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem onClick={fit}>Center map</DropdownMenuItem>
+                <DropdownMenuItem onClick={layout}>Layout map</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 }
 export default FitButton;
