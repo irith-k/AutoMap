d8accd426bfe406fb9664a96716cbffcfa15b26f
Will Renkis <145504791+renkis-byte@users.noreply.github.com>
2025-04-20 19:09:12 -0700

    Update page.js

--- react_flow_front_and_backend/app/page.js
+++ react_flow_front_and_backend/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import ReactFlow, {
   Background,
   Controls,
@@ -7,6 +7,9 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   addEdge,
+  getIncomers,
+  getOutgoers,
+  getConnectedEdges,
 } from "reactflow";
 import "reactflow/dist/style.css";
 
@@ -19,6 +22,7 @@ const MindMap = () => {
 
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [topic, setTopic] = useState("");
 
 
   async function submitMindmapTopic() {
@@ -38,9 +42,64 @@ const MindMap = () => {
     let newedges = response_correct.edges;
     setNodes(newnodes);
     setEdges(newedges);
+    setTopic(topic);
 
 
   }
+
+  async function updateMindmap() {
+    let need = document.getElementById("need").value;
+    if( need.length === 0 ) return;
+
+    let response = await fetch("http://localhost:3002/update-mindmap", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify( {
+        need: need,
+        nodes: nodes,
+        edges: edges,
+        topic: topic,
+      })
+
+    });
+
+    let response_correct = await response.json();
+    let newnodes = response_correct.nodes;
+    let newedges = response_correct.edges;
+    setNodes(newnodes);
+    setEdges(newedges);
+  }
+
+  // The following code was retrieved from official ReactFlow documentation:
+  // https://reactflow.dev/examples/nodes/delete-middle-node
+  const onNodesDelete = useCallback(
+    (deleted) => {
+      setEdges(
+        deleted.reduce((acc, node) => {
+          const incomers = getIncomers(node, nodes, edges);
+          const outgoers = getOutgoers(node, nodes, edges);
+          const connectedEdges = getConnectedEdges([node], edges);
+ 
+          const remainingEdges = acc.filter(
+            (edge) => !connectedEdges.includes(edge),
+          );
+ 
+          const createdEdges = incomers.flatMap(({ id: source }) =>
+            outgoers.map(({ id: target }) => ({
+              id: `${source}->${target}`,
+              source,
+              target,
+            })),
+          );
+ 
+          return [...remainingEdges, ...createdEdges];
+        }, edges),
+      );
+    },
+    [nodes, edges],
+  );
 
    
   const onConnect = useCallback(
@@ -57,12 +116,15 @@ const MindMap = () => {
       <text>Hello! Please enter a mind map topic...</text>
       <input id={"mindmap_topic"} placeholder="Enter topic here"/>
       <button onClick={submitMindmapTopic}>Submit</button>
+      <input id={"need"} placeholder="What do you want changed?"></input>
+      <button onClick={updateMindmap}>Update Mindmap</button>
       <ReactFlow
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
+          onNodesDelete={onNodesDelete}
       fitView
       style={{ backgroundColor: "#F7F9FB" }}
       >
